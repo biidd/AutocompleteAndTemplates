3838b10903caac4f6fdab77a20738b55ddf32f96
biidd <130387329+biidd@users.noreply.github.com>
2023-10-30 23:45:43 +0700

    Add files via upload

---
+++ AutocompleteAndTemplates/src/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Abid", 19);
+        person.displayInfo();
+    }
+}
